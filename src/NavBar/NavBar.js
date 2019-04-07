b48ddb69c32e87ae6d2c37a8a2c625c83a4bb3db
miriamfrank23 <miriamfrank23@gmail.com>
2019-04-07 11:52:15 -0400

    added css modules

---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import style from './NavBar.module.css';
+
+
+const NavBar = () => {
+
+    return (
+      <div className={style.bar}>
+        <div>
+          hiiii
+        </div>
+        <div>
+          hiiii
+        </div>
+        <div>
+          hiiii
+        </div>
+      </div>
+    );
+
+}
+
+export default NavBar;
