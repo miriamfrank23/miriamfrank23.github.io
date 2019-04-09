ae3bbf4b1e37ffba44701b4cb6d478645df18353
miriamfrank23 <miriamfrank23@gmail.com>
2019-04-09 03:00:17 -0400

    basic features live

--- src/Work/Work.js
+++ src/Work/Work.js
@@ -9,12 +9,21 @@ const Work = () => {
       <div className='App'>
         <NavBar />
         <div className={style.main}>
-          Projects
-          <div>
-            <iframe src="https://player.vimeo.com/video/310054916" width="640" height="400" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+          <div className={style.project}>
+            <h3>
+              Book Ends
+            </h3>
+            <div>Web application for exploring, tracking, and chatting about mystery books</div><br/>
+            <a href="https://github.com/miriamfrank23/Book-Ends-frontend">GitHub</a>
+            <iframe src="https://player.vimeo.com/video/310054916" width="640" height="400" frameBorder="0" webkitallowfullscreen='true' mozallowfullscreen='true' allowFullScreen={true}></iframe>
           </div>
-          <div>
-            <iframe src="https://player.vimeo.com/video/310059656" width="640" height="400" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+          <div className={style.project}>
+            <h3>
+              Fantasy Recipes
+            </h3>
+            <div>Fantasy-themed recipe site for sharing Harry Potter, Lord of the Rings, or Star Wars recipes with other fans</div><br/>
+            <a href="https://github.com/miriamfrank23/Fantasy-Recipes-frontend">GitHub</a>
+            <iframe src="https://player.vimeo.com/video/310059656" width="640" height="400" frameBorder="0" webkitallowfullscreen='true' mozallowfullscreen='true' allowFullScreen={true}></iframe>
           </div>
         </div>
       </div>
