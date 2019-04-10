2c7bf8727c44bef4dc84ffde41f6f7c04ecebaba
miriamfrank23 <miriamfrank23@gmail.com>
2019-04-10 01:57:43 -0400

    removed video

--- src/Work/Work.js
+++ src/Work/Work.js
@@ -11,25 +11,30 @@ const Work = () => {
         <NavBar />
         <div className={style.main}>
           <div className={style.project}>
-          <iframe src="https://player.vimeo.com/video/310054916" width="640" height="400" frameBorder="0" webkitallowfullscreen='true' mozallowfullscreen='true' allowFullScreen={true} title='books'></iframe>
             <div className={style.info}>
               <h3>
                 Book Ends
               </h3>
               <a href="https://github.com/miriamfrank23/Book-Ends-frontend">
-              <FontAwesome.FaGithub />
+                <FontAwesome.FaGithub />
+              </a>
+              <a href="https://vimeo.com/310059656">
+                <FontAwesome.FaVimeo />
               </a>
               <div>Web application for exploring, tracking, and chatting about mystery books</div><br/>
             </div>
           </div>
           <div className={style.project}>
-          <iframe src="https://player.vimeo.com/video/310059656" width="640" height="400" frameBorder="0" webkitallowfullscreen='true' mozallowfullscreen='true' allowFullScreen={true} title='recipes'></iframe>
+        
             <div className={style.info}>
               <h3>
                 Fantasy Recipes
               </h3>
               <a href="https://github.com/miriamfrank23/Fantasy-Recipes-frontend">
-              <FontAwesome.FaGithub />
+                <FontAwesome.FaGithub />
+              </a>
+              <a href="https://vimeo.com/310054916">
+                <FontAwesome.FaVimeo />
               </a>
               <div>Fantasy-themed recipe site for sharing Harry Potter, Lord of the Rings, or Star Wars recipes with other fans</div><br/>
             </div>
@@ -40,7 +45,6 @@ const Work = () => {
                 Single Player SET
               </h3>
               <div>TBD</div><br/>
-              <a href="https://github.com/miriamfrank23/Set-Clone-frontend">GitHub</a>
             </div>
           </div>
         </div>
